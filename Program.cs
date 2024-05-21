e8de9a91c3ce05178a47af455b24e65e2cef8e4a
Ayush Rawat <91361894+Ayurt@users.noreply.github.com>
2024-05-21 14:45:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+     static void Main(string[] args)
+        {
+            Console.Write("Enter the height of the triangle: ");
+            if (int.TryParse(Console.ReadLine(), out int height) && height > 0)
+            {
+                PrintTriangle(height);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid positive integer for the height.");
+            }
+        }
+        static void PrintTriangle(int height)
+        {
+           for (int i = 1; i <= height; ++i)
+            {
+                for (int space = 1; space <= height - i; ++space)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k != 2 * i - 1; ++k)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+}
