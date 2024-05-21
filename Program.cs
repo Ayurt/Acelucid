1ac1e552b8beba180517221038ad8fdceb1aa363
Ayush Rawat <91361894+Ayurt@users.noreply.github.com>
2024-05-21 14:47:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DateFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string:");
+            string input = Console.ReadLine();
+
+            string datePattern = @"(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])\d{4}";
+            Match match = Regex.Match(input, datePattern);
+
+            if (match.Success)
+            {
+                Console.WriteLine("Found date: " + match.Value);
+            }
+            else
+            {
+                Console.WriteLine("No valid date found in the input string.");
+            }
+        }
+    }
+}
